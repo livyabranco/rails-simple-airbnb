4c382dabcbf1f07b497016151e037ea63b596c17
Livya Branco <livyabranco@gmail.com>
2022-07-19 18:13:18 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light in Paris',
+  address: '75008 Paris',
+  description: 'A lovely french appart',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Rio',
+  address: 'Ipanema',
+  description: 'Sun',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Madrid',
+  address: 'Spain street',
+  description: 'Sun and sun',
+  price_per_night: 75,
+  number_of_guests: 3
+)
